3b23d36a4e0995999dde8323c959f5d78a004621
vsai911 <varunsai91@gmail.com>
2016-04-17 01:59:02 +0000

    added routes for team page

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/portfolio', function(req,res){
   res.sendFile(__dirname + '/public/portfolio.html');
 });
 
+app.get('/team', function(req,res){
+  res.sendFile(__dirname + '/public/team.html');
+});
+
 app.get('/', function (req, res) {
   res.send('root');
 });
@@ -14,4 +18,3 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
