e68a7538d9d7d8575ef11f239e937441ba756f9f
Lance Barlaan <lbarlaan@appbridge.ca>
2016-04-16 18:10:50 -0700

    + basic routing for portfolio/projects page

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/portfolio', function(req,res){
+  res.sendFile(__dirname + '/public/portfolio.html');
+});
+
+app.get('/', function (req, res) {
+  res.send('root');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
