349c2f9686c76096f2a78f182e1538b276f36ad9
Tulun <jaykiraly@gmail.com>
2016-04-22 15:48:45 -0700

    Basic parallax works.

--- public/lib/_main.js
+++ public/lib/_main.js
@@ -1,11 +1,11 @@
-/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
-(function(n){n.viewportSize={},n.viewportSize.getHeight=function(){return t("Height")},n.viewportSize.getWidth=function(){return t("Width")};var t=function(t){var f,o=t.toLowerCase(),e=n.document,i=e.documentElement,r,u;return n["inner"+t]===undefined?f=i["client"+t]:n["inner"+t]!=i["client"+t]?(r=e.createElement("body"),r.id="vpw-test-b",r.style.cssText="overflow:scroll",u=e.createElement("div"),u.id="vpw-test-d",u.style.cssText="position:absolute;top:-1000px",u.innerHTML="<style>@media("+o+":"+i["client"+t]+"px){body#vpw-test-b div#vpw-test-d{"+o+":7px!important}}<\/style>",r.appendChild(u),i.insertBefore(r,e.head),f=u["offset"+t]==7?i["client"+t]:n["inner"+t],i.removeChild(r)):f=n["inner"+t],f}})(this);
-
-/**
- * How to create a parallax scrolling website
- * Author: Petr Tichy
- * URL: www.ihatetomatoes.net
- * Article URL: http://ihatetomatoes.net/how-to-create-a-parallax-scrolling-website/
+ /**
+ * This demo was prepared for you by Petr Tichy - Ihatetomatoes.net
+ * Want to see more similar demos and tutorials?
+ * Help by spreading the word about Ihatetomatoes blog.
+ * Facebook - https://www.facebook.com/ihatetomatoesblog
+ * Twitter - https://twitter.com/ihatetomatoes
+ * Google+ - https://plus.google.com/u/0/109859280204979591787/about
+ * Article URL: http://ihatetomatoes.net/simple-parallax-scrolling-tutorial/
  */
 
 ( function( $ ) {
@@ -13,8 +13,6 @@
   // Setup variables
   $window = $(window);
   $slide = $('.homeSlide');
-  $slideTall = $('.homeSlideTall');
-  $slideTall2 = $('.homeSlideTall2');
   $body = $('body');
   
     //FadeIn all sections   
@@ -34,11 +32,7 @@
     
     // Init Skrollr
     var s = skrollr.init({
-        render: function(data) {
-        
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
-        }
+        forceHeight: false
     });
     
     // Get window size
@@ -51,8 +45,6 @@
       
       // Resize our slides
       $slide.height(winH);
-      $slideTall.height(winH*2);
-      $slideTall2.height(winH*3);
       
       // Refresh Skrollr after resizing our sections
       s.refresh($('.homeSlide'));
