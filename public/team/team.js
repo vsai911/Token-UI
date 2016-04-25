cf53320249334e0f850f43facf9c78073ae06370
Lance Barlaan <LPTBarlaan@gmail.com>
2016-04-25 00:53:30 -0700

    Modal experimentation

--- public/team/team.js
+++ public/team/team.js
@@ -1,3 +1,9 @@
-particlesJS.load('particles-js', 'particles.json', function() {
+$(function(){
+  particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
+  });
+
+  $('#Sai').on('click',function (evt){
+    
+  });
 });
