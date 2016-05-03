7848985c264860b1c0df95b28bac286ee7b9967a
Axat <akshatdivekar@gmail.com>
2016-05-02 22:02:56 -0700

    Added drawLine function for mobile devices

--- public/team/team.js
+++ public/team/team.js
@@ -1,29 +1,18 @@
-// <<<<<<< HEAD
-// $(function(){
-//   particlesJS.load('particles-js', 'particles.json', function() {
-//   console.log('callback - particles.js config loaded');
-//   });
-
-//   $('#Sai').on('click',function (evt){
-    
-//   });
-// });
-// =======
 var TEAM_POLYGON_RADIUS = 250;
-var TOGGLE_DURATION = 500;
-
-// // Classes
+// TODO: change this later
+var POLYGON_MIN_WIDTH = 700;
+// Classes
 var TEAM_MEMBER = '.team-member';
 
 function drawPolygon() {
 	//Draw the polygon
 	var radius = TEAM_POLYGON_RADIUS;
-	var fields = $(TEAM_MEMBER), 
+	var $fields = $(TEAM_MEMBER),
 		container = $('#team-container'), 
 		width = container.width(), 
 		height = container.height();
-	var angle = 0, step = (2*Math.PI) / fields.length;
-	fields.each(function() {
+	var angle = 0, step = (2*Math.PI) / $fields.length;
+	$fields.each(function() {
 	    var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2);
 	    var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2);
 	    $(this).css({
@@ -34,16 +23,47 @@ function drawPolygon() {
 	});
 }
 
-$(document).ready(function() {
-	particlesJS.load('particles-js', 'particles.json', function() {
-  		console.log('callback - particles.js config loaded');
+function drawLine() {
+	var $fields = $(TEAM_MEMBER),
+		container = $('#team-container'),
+		containerWidth = container.width() / 2,
+		margin = 20;
+
+	function getY(index) {
+		if (index === 0) {
+			return margin;
+		}
+		var $previous = $($fields[index - 1]);	
+		return $previous.position().top + $previous.height() + margin;
+	}
+
+	$fields.each(function(index) {
+	    var x = containerWidth - ($(this).width() / 2);
+	    var y =  getY(index);
+	    $(this).css({
+	        left: x + 'px',
+	        top: y + 'px'
+	    });
 	});
-	// $(TEAM_MEMBER).click(function() {
-	// 	$('#member-information').toggle(TOGGLE_DURATION);
-	// 	$(this).toggleClass('team-member-flip');
-	// });
-	drawPolygon();
+}
+
+function getViewportWidth() {
+	return (window.innerWidth > 0) ? window.innerWidth : screen.width;
+}
+
+function refreshTeamView() {
+	// Determine window inner width to draw polygon
+	if (getViewportWidth() >= POLYGON_MIN_WIDTH) {
+		drawPolygon();
+	} else {
+		drawLine();
+	}
+}
+
+
+$(document).ready(function() {
+	particlesJS.load('particles-js', 'particles.json');
+	refreshTeamView();
 });
 
-$(window).on('resize', drawPolygon);
-// >>>>>>> a69e0714c3c2a0b81f5d58f42d073e79bdcb0d9e
+$(window).on('resize', refreshTeamView);
