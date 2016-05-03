c0d2bf5167ce8ab5c7e40ea68e8ed41859803c05
veebuv <veebuv@gmail.com>
2016-05-04 02:37:48 +1000

    [FEATURE] node mailer

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var app = express();
+var emailJS = require('emailjs/email')
+var bodyParser = require('body-parser');
+
+// create application/json parser 
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use('/',jsonParser);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -15,6 +25,47 @@ app.get('/', function (req, res) {
   res.send('root');
 });
 
+app.get('/contact', function(req,res){
+  res.sendFile(__dirname + '/public/form/form.html');
+});
+
+app.post('/contact-submit',function(req,res){
+	var requestBody = (req.body);
+	['veebuv@gmail.com'].forEach(function(email){
+		sendEmailTo(email,requestBody.name,requestBody.email,requestBody.message,req,res)
+	})
+	
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+//************NODE EMAIL
+var emailJS = require('emailjs/email')
+
+var sendmail = emailJS.server.connect({
+    user: 'teamdreamstream4@gmail.com',
+    password: 'teamdreamstream',
+    host: "smtp.gmail.com",
+    ssl: true
+});
+
+function sendEmailTo (email_id,client_name,client_email,message,req,res){
+    var message = {
+
+        from: "Zenkara <teamdreamstream4@gmail.com>",
+        to: "User <" + email_id + ">",
+        subject: client_name + " wants to chat with us!",
+        text: "They can be reached on: " + client_email + " and they have a message : \n" + message
+    };
+
+    sendmail.send(message, function (err, message) {
+        var body = null;
+        if (err) {
+            body = err.toString();
+        } else {
+            console.log("EMAIL SENT IN SERVER")
+        }
+    });
+}
