00d1fdfec25c55c62bf1e941c4fca83a6aebc9b0
Tulun <jaykiraly@gmail.com>
2016-06-09 14:22:04 -0700

    Added basic webpack rendering.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+
+const PATHS = {
+  app: path.join(__dirname, 'public/app'),
+  build: path.join(__dirname, 'public/build')
+};
+
+const common = {
+  // Entry accepts a path or an object of entries.
+  // We'll be using the latter form given it's
+  // convenient with more complex configurations.
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    title: 'Webpack demo'
+  })
+  ]
+};
+
+var config;
+  // Detect how npm is run and branch based on that
+  switch(process.env.npm_lifecycle_event) {
+    case 'build':
+      config = merge(common, {});
+      break;
+    default:
+      config = merge(common, {});
+};
+
+module.exports = validate(config);
