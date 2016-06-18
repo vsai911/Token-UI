f16c651af8dbeacad8e63f9ee737b96745296ec5
Tulun <jaykiraly@gmail.com>
2016-06-18 12:56:47 -0700

    Fixed bug with contact form on index.

--- public/app/index.js
+++ public/app/index.js
@@ -6,47 +6,47 @@
 
 // Parallax doesn't seem to do anything -- please test. Commented out for now.
 
-// (function($) {
+(function($) {
 
-//   $.fn.parallax = function(options) {
+  $.fn.parallax = function(options) {
 
-//     var windowHeight = $(window).height();
+    var windowHeight = $(window).height();
 
-//     // Establish default settings
-//     var settings = $.extend({
-//         speed        : 0.15
-//     }, options);
+    // Establish default settings
+    var settings = $.extend({
+        speed        : 0.15
+    }, options);
 
-//     // Iterate over each object in collection
-//     return this.each( function() {
+    // Iterate over each object in collection
+    return this.each( function() {
 
-//     	// Save a reference to the element
-//     	var $this = $(this);
+    	// Save a reference to the element
+    	var $this = $(this);
 
-//     	// Set up Scroll Handler
-//     	$(document).scroll(function(){
+    	// Set up Scroll Handler
+    	$(document).scroll(function(){
 
-// 		    var scrollTop = $(window).scrollTop();
-//         var offset = $this.offset().top;
-//         var height = $this.outerHeight();
+		    var scrollTop = $(window).scrollTop();
+        var offset = $this.offset().top;
+        var height = $this.outerHeight();
 
-//       	// Check if above or below viewport
-//     		if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
-//     			return;
-//     		}
+      	// Check if above or below viewport
+    		if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+    			return;
+    		}
 
-//   		  var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
-//         // Apply the Y Background Position to Set the Parallax Effect
-//   			$this.css('background-position', 'center ' + yBgPosition + 'px');
-//       });
-//     });
-//   }
+  		  var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+        // Apply the Y Background Position to Set the Parallax Effect
+  			$this.css('background-position', 'center ' + yBgPosition + 'px');
+      });
+    });
+  }
 
-// }(jQuery));
+}(jQuery));
 
-// $('.parallax').parallax({
-// 	speed :	0.15
-// });
+$('.parallax').parallax({
+	speed :	0.15
+});
 
 
 $(document).ready(function(){
@@ -70,4 +70,8 @@ $(document).ready(function(){
 
     $('#contact-form').toggle(effect, options, duration);
   });
+})
+
+$('.navbar-toggle').click(function(){
+  console.log(this);
 })
